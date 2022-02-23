4587b38f0ead8f6f8fea80323a22e80d27670173
unknown <nidhisingh25901@gmail.com>
2022-02-23 22:22:50 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Snake-Game
+<!-- ALL-CONTRIBUTORS-BADGE:START - Do not remove or modify this section -->
+[![All Contributors](https://img.shields.io/badge/all_contributors-6-orange.svg?style=flat-square)](#contributors-)
+<!-- ALL-CONTRIBUTORS-BADGE:END -->
+## Contributors ✨
+
+Thanks goes to these wonderful people :
+
+<!-- ALL-CONTRIBUTORS-LIST:START - Do not remove or modify this section -->
+<!-- prettier-ignore-start -->
+<!-- markdownlint-disable -->
+<table>
+  <tr>
+    <td align="center"><a href="https://github.com/TDP17"><img src="https://avatars.githubusercontent.com/u/71214045?v=4?s=100" width="100px;" alt=""/><br /><sub><b>TDP17</b></sub></a><br /><a href="https://github.com/Bauddhik-Geeks/Snake-Game/commits?author=TDP17" title="Code">💻</a></td>
+    <td align="center"><a href="http://shashwatmishra.codes/"><img src="https://avatars.githubusercontent.com/u/56419358?v=4?s=100" width="100px;" alt=""/><br /><sub><b>Shashwat Mishra</b></sub></a><br /><a href="https://github.com/Bauddhik-Geeks/Snake-Game/commits?author=ampsteric" title="Code">💻</a></td>
+    <td align="center"><a href="https://github.com/OmDharme"><img src="https://avatars.githubusercontent.com/u/85190369?v=4?s=100" width="100px;" alt=""/><br /><sub><b>OmDharme</b></sub></a><br /><a href="https://github.com/Bauddhik-Geeks/Snake-Game/commits?author=OmDharme" title="Code">💻</a></td>
+    <td align="center"><a href="https://github.com/aanchal-fatwani"><img src="https://avatars.githubusercontent.com/u/63228776?v=4?s=100" width="100px;" alt=""/><br /><sub><b>Aanchal</b></sub></a><br /><a href="https://github.com/Bauddhik-Geeks/Snake-Game/commits?author=aanchal-fatwani" title="Code">💻</a></td>
+    <td align="center"><a href="https://gourav-saini.netlify.app/"><img src="https://avatars.githubusercontent.com/u/46393634?v=4?s=100" width="100px;" alt=""/><br /><sub><b>Gourav Saini</b></sub></a><br /><a href="https://github.com/Bauddhik-Geeks/Snake-Game/commits?author=g7i" title="Code">💻</a></td>
+    <td align="center"><a href="https://github.com/ayush78781"><img src="https://avatars.githubusercontent.com/u/76848435?v=4?s=100" width="100px;" alt=""/><br /><sub><b>Ayush Saxena</b></sub></a><br /><a href="https://github.com/Bauddhik-Geeks/Snake-Game/commits?author=ayush78781" title="Code">💻</a></td>
+  </tr>
+</table>
+
+<!-- markdownlint-restore -->
+<!-- prettier-ignore-end -->
+
+<!-- ALL-CONTRIBUTORS-LIST:END -->
+
+This project follows the [all-contributors](https://github.com/all-contributors/all-contributors) specification. Contributions of any kind welcome!
---
+++ snake.js
@@ -0,0 +1,202 @@
+const snakeColor = document.getElementById('snake-color');
+	// this Function will run only once
+function init(){
+	canvas = document.getElementById('mycanvas');
+	W = H = canvas.width = canvas.height = 1000;
+	pen = canvas.getContext('2d');
+	cs = 66;
+	game_over = false;
+	score = 5;
+
+	//Adds listener to reset high score
+	resetButton = document.getElementById('reset-high-score-btn');
+	resetButton.addEventListener('click', () => {
+		localStorage.removeItem('highScore');
+		high_score = localStorage.getItem('highScore') || 5;
+		highScoreText.innerText = `High score: ${high_score}`;
+	}, { once: true });
+
+	//Get highscore from localstorage and update display
+	highScoreText = document.getElementById('high-score');
+	high_score = localStorage.getItem('highScore') || 5;
+	highScoreText.innerText = `High score: ${high_score}`;
+
+	//Create a Image Object for food
+	food_img = new Image();
+	food_img.src = "Assets/foods/food-1.png";
+
+	trophy = new Image();
+	trophy.src = "Assets/trophy.png";
+
+	food = getRandomFood(); // getting random cordinates for food
+
+	snake = {
+		init_len:5,
+		color: snakeColor.value,
+		cells:[],
+		direction:"right",
+
+
+		createSnake:function(){
+			for(var i=this.init_len;i>0;i--){
+				this.cells.push({x:i,y:0});
+			}
+		},
+		drawSnake:function(){
+
+			for(var i=0;i<this.cells.length;i++){
+				pen.fillStyle = this.color;
+				pen.fillRect(this.cells[i].x*cs,this.cells[i].y*cs,cs-3,cs-3);
+			}
+		},
+
+		updateSnake:function(){
+							//console.log("updating snake according to the direction property");
+							//check if the snake has eaten food, increase the length of the snake and
+							//generate new food object
+			var headX = this.cells[0].x;
+			var headY = this.cells[0].y;
+
+			if(headX==food.x && headY==food.y){
+				console.log("Food eaten");
+				food = getRandomFood();
+				score++;
+
+			}
+			else
+			{
+				this.cells.pop();
+			}
+
+
+			var nextX,nextY;
+
+			if(this.direction=="right"){
+				nextX = headX + 1;
+				nextY = headY;
+			}
+			else if(this.direction=="left"){
+				nextX = headX - 1;
+				nextY = headY;
+			}
+			else if(this.direction=="down"){
+				nextX = headX;
+				nextY = headY + 1;
+			}
+			else{
+				nextX = headX;
+				nextY = headY - 1;
+			}
+
+			this.cells.unshift({x: nextX,y:nextY});
+
+						/*Logic that prevents snake from going out*/
+			var last_x = Math.round(W/cs);
+			var last_y = Math.round(H/cs);
+
+			if(this.cells[0].y<0 || this.cells[0].x < 0 || this.cells[0].x > last_x || this.cells[0].y > last_y){
+				game_over = true;
+			}
+
+		}
+
+	};
+
+	snake.createSnake();
+					//Add a Event Listener on the Document Object
+	function keyPressed(e){			// This will be returning some extra things but we only need key to access which key is pressed by a user
+					//Conditional Statments
+		if(e.key=="ArrowRight"){
+			snake.direction = "right";		//for right key
+		}
+		else if(e.key=="ArrowLeft"){
+			snake.direction = "left";		// for left key
+		}
+		else if(e.key=="ArrowDown"){
+			snake.direction = "down";		// for down key
+		}
+		else{
+			snake.direction = "up";			// for up key
+		}
+		console.log(snake.direction);
+	}
+
+
+	document.addEventListener('keydown',keyPressed) ;
+
+}
+
+
+function draw(){
+						//console.log("In Draw");
+
+						//erasing the old frame
+	pen.clearRect(0,0,W,H);
+	snake.drawSnake();
+
+	pen.fillStyle = food.color;
+	pen.drawImage(food_img,food.x*cs,food.y*cs,cs,cs);
+
+	pen.drawImage(trophy,18,20,cs,cs);
+	pen.fillStyle = "blue";
+	pen.font = "20px Roboto"
+	pen.fillText(score,50,50);
+
+
+}
+
+function update(){
+				//console.log("In Update");
+	snake.updateSnake();
+}
+
+function getRandomFood(){
+
+	var foodX = Math.round(Math.random()*(W-cs)/cs); 		//getting random food value's X cordinate
+	var foodY = Math.round(Math.random()*(H-cs)/cs);		// now I'm getting random value of Y cordinate
+
+	const foodNumber = Math.trunc(Math.random() * 4) + 1;
+	food_img.src = `Assets/foods/food-${foodNumber}.png`;
+
+	var food = {
+		x:foodX,
+		y:foodY,
+		color:"red",
+	}
+	return food;				// returning food with random food values(i.e, cordinates)
+
+}
+
+function gameloop(){					// will run until game is not over
+	if(game_over==true){
+		if (score > high_score) {
+			high_score = score;
+		}
+		highScoreText.innerText = `High score: ${high_score}`;  // Updates high score
+		localStorage.setItem('highScore', high_score); // Sets high score in LS
+		clearInterval(intervalId);
+		document.getElementById("start").innerText = "Restart Game"
+		alert("Game Over");			// popup for game over when it touches the boundary
+		return;
+	}
+	draw();
+	update();
+}
+
+var intervalId;
+document.getElementById("start").addEventListener("click", () => {
+	clearInterval(intervalId)
+	init();					// that top init function is called at first once
+	intervalId = setInterval(gameloop, 100); // setting interval of 100 milisecond
+});
+
+
+
+
+
+
+
+
+
+
+
